e37550cd38e5e8acc771186371eeaf710a5cfdbf
wang-guolong1 <3192285717@qq.com>
2024-09-09 21:22:53 +0800

    测试提交

---
+++ test_24_06_25/test.c
@@ -0,0 +1,77 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//写一个C语言代码，打印helloworld
+
+//standard input output 
+#include<stdio.h>
+//int main() {
+//	/*printf("hello bit\n");
+//	printf("我真帅");*/
+//	//printf("hello\nworld");
+//	/*
+//	printf("%d\n", sizeof(char));
+//	printf("%d\n", sizeof(short));
+//	printf("%d\n", sizeof(int));
+//	printf("%d\n", sizeof(float));
+//	printf("%d\n", sizeof(long));
+//	printf("%d\n", sizeof(long long));
+//	printf("%d\n", sizeof(double));*/
+//	char ch = 'w';
+//	printf("%c\n", ch);
+//	printf("%d\n", sizeof(ch));
+//	float weight = 75.5f;  //4byte
+//	double pi = 3.14; //8byte
+//	//int age = 18;
+//	double salary = 20000.0;
+//	//int m;//不是好习惯  ,给一个初始值
+//	int n = 0;
+//	//printf("%d\n", m);
+//	char a_c = 'w';
+//	int age=18;
+//	int Age=19;
+//	printf("%d\n", age);
+//	printf("%d\n", Age);
+//		return 0;
+//}int c = 15;
+//printf()打印函数，上来能直接用   io
+// //int main(void) {
+//	printf("hehe");
+//	return 0;
+//}
+//void main() {
+//	printf("hehe\n");
+//		//return 0;
+//}
+///*
+//int main() {
+//	printf("hello bit\n");
+//	return 0;
+//}
+//
+//*/
+//int  c = 20;
+//int a = 10;
+//int main() {
+//	int b = 20;
+//	a = 12;
+//	printf("%d\n", a);  
+//	printf("%d\n", b);
+//	//printf("%d\n", c);
+//	return 0;
+//}
+/*int a = 10;
+int main() {
+	 a = 1;
+	printf("%d\n", a);
+	return 0;
+}*/
+//写一个代码计算和
+
+int main() {
+	int num1 = 0, num2 = 0, sum = 0;
+	scanf("%d %d", &num1, &num2);
+	sum = num1 + num2;
+	printf("sum= %d", sum);
+	return 0;
+}
+
+
